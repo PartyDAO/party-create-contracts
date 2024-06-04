1c384ac90003af960a3c20666dda2b331655fd56
Arr00 <13561405+arr00@users.noreply.github.com>
2024-06-04 10:48:42 -0400

    feat: allow owner spending tokens without approval (#2)

--- src/CircuitBreakerERC20.sol
+++ src/CircuitBreakerERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8;
+pragma solidity ^0.8.25;
 
 import { ERC20Permit, Nonces } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import { ERC20Votes, ERC20 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
@@ -27,12 +27,24 @@ contract CircuitBreakerERC20 is ERC20Permit, ERC20Votes, Ownable {
         _mint(_receiver, _totalSupply);
     }
 
-    // The following functions are overrides required by Solidity.
+    /// @notice Only owner can transfer functions when paused. They can transfer out or call `transferFrom` to
+    /// themselves.
     function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Votes) {
-        if (paused) revert TokenPaused();
+        address owner = owner();
+        if (paused && from != owner && (to != owner || msg.sender != owner)) {
+            revert TokenPaused();
+        }
         super._update(from, to, value);
     }
 
+    /// @dev Enable owner to spend tokens without approval.
+    function _spendAllowance(address tokenOwner, address tokenSpender, uint256 value) internal override(ERC20) {
+        if (tokenSpender != owner()) {
+            super._spendAllowance(tokenOwner, tokenSpender, value);
+        }
+    }
+
+    // The following functions are overrides required by Solidity.
     function nonces(address owner) public view override(ERC20Permit, Nonces) returns (uint256) {
         return super.nonces(owner);
     }
