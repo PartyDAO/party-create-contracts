baace3706220847b1f83bb5aeacd6cfb8eb8ba33
Arr00 <13561405+arr00@users.noreply.github.com>
2024-06-10 15:28:52 -0400

    Distribute WETH instead of ETH

--- src/NFTBoundLPLocker.sol
+++ src/NFTBoundLPLocker.sol
@@ -3,13 +3,11 @@ pragma solidity ^0.8.25;
 
 import { INonfungiblePositionManager } from "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
 import { IERC721Receiver } from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { IWETH } from "./external/IWETH.sol";
 import { IUNCX } from "./external/IUNCX.sol";
 
-contract NFTBoundLPLocker is IERC721Receiver, ReentrancyGuard {
+contract NFTBoundLPLocker is IERC721Receiver {
     error OnlyPositionManager();
     error InvalidFeeBps();
 
@@ -33,15 +31,13 @@ contract NFTBoundLPLocker is IERC721Receiver, ReentrancyGuard {
     }
 
     INonfungiblePositionManager public immutable POSITION_MANAGER;
-    IWETH public immutable WETH;
     IERC721 public immutable PARTY_TOKEN_ADMIN;
     IUNCX public immutable UNCX;
 
     mapping(uint256 => LPInfo) public lpInfos;
 
-    constructor(INonfungiblePositionManager positionManager, IWETH weth, IERC721 partyTokenAdmin, IUNCX uncx) {
+    constructor(INonfungiblePositionManager positionManager, IERC721 partyTokenAdmin, IUNCX uncx) {
         POSITION_MANAGER = positionManager;
-        WETH = weth;
         PARTY_TOKEN_ADMIN = partyTokenAdmin;
         UNCX = uncx;
     }
@@ -94,57 +90,28 @@ contract NFTBoundLPLocker is IERC721Receiver, ReentrancyGuard {
         return IERC721Receiver.onERC721Received.selector;
     }
 
-    function collect(uint256 lockId) external nonReentrant {
+    function collect(uint256 lockId) external {
         LPInfo memory lpInfo = lpInfos[lockId];
 
         (uint256 amount0, uint256 amount1,,) = UNCX.collect(lockId, address(this), type(uint128).max, type(uint128).max);
-
-        if (lpInfo.token0 == address(WETH)) {
-            WETH.withdraw(amount0);
-        }
-        if (lpInfo.token1 == address(WETH)) {
-            WETH.withdraw(amount1);
-        }
 
         for (uint256 i = 0; i < lpInfo.additionalFeeRecipients.length; i++) {
             AdditionalFeeRecipient memory recipient = lpInfo.additionalFeeRecipients[i];
 
             if (recipient.feeType == FeeType.Token0 || recipient.feeType == FeeType.Both) {
-                if (lpInfo.token0 == address(WETH)) {
-                    recipient.recipient.call{ value: amount0 * recipient.percentageBps / 10_000, gas: 10_000 }("");
-                } else {
-                    IERC20(lpInfo.token0).transfer(recipient.recipient, amount0 * recipient.percentageBps / 10_000);
-                }
+                IERC20(lpInfo.token0).transfer(recipient.recipient, amount0 * recipient.percentageBps / 10_000);
             }
 
             if (recipient.feeType == FeeType.Token1 || recipient.feeType == FeeType.Both) {
-                if (lpInfo.token1 == address(WETH)) {
-                    recipient.recipient.call{ value: amount1 * recipient.percentageBps / 10_000, gas: 10_000 }("");
-                } else {
-                    IERC20(lpInfo.token1).transfer(recipient.recipient, amount1 * recipient.percentageBps / 10_000);
-                }
+                IERC20(lpInfo.token1).transfer(recipient.recipient, amount1 * recipient.percentageBps / 10_000);
             }
         }
 
         address remainingReceiver = PARTY_TOKEN_ADMIN.ownerOf(lpInfo.partyTokenAdminId);
 
-        if (lpInfo.token0 == address(WETH)) {
-            remainingReceiver.call{ value: address(this).balance, gas: 10_000 }("");
-        } else {
-            IERC20(lpInfo.token0).transfer(remainingReceiver, IERC20(lpInfo.token0).balanceOf(address(this)));
-        }
-
-        if (lpInfo.token1 == address(WETH)) {
-            remainingReceiver.call{ value: address(this).balance, gas: 10_000 }("");
-        } else {
-            IERC20(lpInfo.token1).transfer(remainingReceiver, IERC20(lpInfo.token1).balanceOf(address(this)));
-        }
+        IERC20(lpInfo.token0).transfer(remainingReceiver, IERC20(lpInfo.token0).balanceOf(address(this)));
+        IERC20(lpInfo.token1).transfer(remainingReceiver, IERC20(lpInfo.token1).balanceOf(address(this)));
     }
-
-    /**
-     * @notice Enable unwrapping WETH
-     */
-    receive() external payable { }
 
     /**
      * @dev Returns the version of the contract. Minor versions indicate change in logic. Major version indicates
