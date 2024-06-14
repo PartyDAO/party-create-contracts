1fb6f4057cdee188c06b95ce28d2de78108eed69
Arr00 <13561405+arr00@users.noreply.github.com>
2024-06-14 13:25:19 -0400

    feat: contract URI (#15)
    * feat: contract URI
    
    * fix test
    
    * add test

--- test/PartyTokenAdminERC721.t.sol
+++ test/PartyTokenAdminERC721.t.sol
@@ -54,7 +54,17 @@ contract PartyTokenAdminERC721Test is Test, LintJSON {
         adminNft.setLaunchSucceeded(tokenId);
     }
 
+    event ContractURIUpdated();
+
+    function test_setContractURI() external {
+        vm.expectEmit(true, true, true, true);
+        emit ContractURIUpdated();
+        adminNft.setContractURI("test_contract_uri");
+
+        assertEq(adminNft.contractURI(), "test_contract_uri");
+    }
+
     function test_VERSION() external {
-        assertEq(adminNft.VERSION(), "0.3.0");
+        assertEq(adminNft.VERSION(), "0.4.0");
     }
 }
