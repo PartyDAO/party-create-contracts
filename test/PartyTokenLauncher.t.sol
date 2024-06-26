0ca0d785161029596d96fcceaab39db47de181ee
Arr00 <13561405+arr00@users.noreply.github.com>
2024-06-26 14:08:32 -0400

    fees are 2.5% or lower (#58)

--- test/PartyTokenLauncher.t.sol
+++ test/PartyTokenLauncher.t.sol
@@ -143,6 +143,51 @@ contract PartyTokenLauncherTest is Test {
         assertTrue(launch.getLaunchLifecycle(launchId) == PartyTokenLauncher.LaunchLifecycle.Finalized);
     }
 
+    function test_createLaunch_invalidFee() external {
+        address creator = vm.createWallet("Creator").addr;
+        address recipient = vm.createWallet("Recipient").addr;
+        vm.deal(creator, 1 ether);
+
+        PartyTokenLauncher.LockerFeeRecipient[] memory lockerFeeRecipients =
+            new PartyTokenLauncher.LockerFeeRecipient[](1);
+        lockerFeeRecipients[0] = PartyTokenLauncher.LockerFeeRecipient({
+            recipient: vm.createWallet("AdditionalLPFeeRecipient").addr,
+            bps: 1e4
+        });
+
+        PartyTokenLauncher.ERC20Args memory erc20Args = PartyTokenLauncher.ERC20Args({
+            name: "NewToken",
+            symbol: "NT",
+            image: "image_url",
+            description: "New Token Description",
+            totalSupply: 1_000_000 ether
+        });
+
+        PartyTokenLauncher.LaunchArgs memory launchArgs = PartyTokenLauncher.LaunchArgs({
+            numTokensForLP: 500_000 ether,
+            numTokensForDistribution: 300_000 ether,
+            numTokensForRecipient: 200_000 ether,
+            targetContribution: 10 ether,
+            maxContributionPerAddress: 8 ether,
+            merkleRoot: bytes32(0),
+            recipient: recipient,
+            finalizationFeeBps: 251,
+            withdrawalFeeBps: withdrawalFeeBps,
+            lockerFeeRecipients: lockerFeeRecipients
+        });
+
+        vm.prank(creator);
+        vm.expectRevert(PartyTokenLauncher.InvalidFee.selector);
+        launch.createLaunch{ value: 1 ether }(erc20Args, launchArgs);
+
+        launchArgs.finalizationFeeBps = 0;
+        launchArgs.withdrawalFeeBps = 251;
+
+        vm.prank(creator);
+        vm.expectRevert(PartyTokenLauncher.InvalidFee.selector);
+        launch.createLaunch{ value: 1 ether }(erc20Args, launchArgs);
+    }
+
     function test_contribute_works() public {
         uint32 launchId = test_createLaunch_works();
         address contributor = vm.createWallet("Contributor").addr;
