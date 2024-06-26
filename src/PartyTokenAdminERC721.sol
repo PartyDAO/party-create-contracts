243c45df4106b5cdf3339c3627d23bd09843e405
Arr00 <13561405+arr00@users.noreply.github.com>
2024-06-26 14:01:19 -0400

    mitigation: I-1 metadata updated event (#45)

--- src/PartyTokenAdminERC721.sol
+++ src/PartyTokenAdminERC721.sol
@@ -122,6 +122,7 @@ contract PartyTokenAdminERC721 is ERC721, Ownable, IERC4906 {
         tokenMetadatas[tokenId].image = image;
 
         emit TokenImageSet(tokenId, image);
+        emit MetadataUpdate(tokenId);
     }
 
     /**
